8a6eb50a605e2e98e2e59b96578351494522db38
edenha <eden.r.hartman@gmail.com>
2022-11-03 01:27:19 +0200

    ex1.1

--- ex1/main.py
+++ ex1/main.py
@@ -7,15 +7,27 @@ def f(x: int, y: float, z):
 
 
 def safe_call(func, **kwargs):
+    """ this function gets another function and args for it
+        >>> safe_call(f, x=1, y=1.1, z=2)
+        4.1
+        >>> safe_call(f, x=1, y=1.5, z=2)
+        4.5
+        >>> safe_call(f, x=1, y=1, z=2)
+        Traceback (most recent call last):
+        ...
+        Exception: invalid arg type: y, expected <class 'float'> and got <class 'int'>
+    """
     args = inspect.getfullargspec(func).args
     annotations = inspect.getfullargspec(func).annotations
     for a in args:
         if a in annotations.keys() and annotations[a] is not type(kwargs[a]):
             raise Exception(f"invalid arg type: {a}, expected {annotations[a]} and got {type(kwargs[a])}")
-    print("all good")
     print(f(**kwargs))
 
 
 if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod(verbose=True)
+
     # safe_call(f, x=1, y=2, z=3)
-    safe_call(f, x=1, y=2.2, z=3)
+    safe_call(f, x=1, y=1.1, z=2)
